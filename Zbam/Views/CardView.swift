82432f0a30163f04954fbe62c2202152d9b33b0b
ygteker <gunes.teker@gmail.com>
2026-01-18 23:09:19 +0100

    added card

--- Zbam/Views/CardView.swift
+++ Zbam/Views/CardView.swift
@@ -10,20 +10,34 @@ import SwiftData
 
 struct CardView: View {
     let card: Card
-    @State private var answer: String
     @State private var isPresentingEditView: Bool = false
+    @State private var flipped: Bool = false
     
     init(card: Card) {
         self.card = card
-        self._answer = State(initialValue: card.front)
     }
     
     var body: some View {
+        let flipDegrees = flipped ? 180.0 : 0
         VStack {
             NavigationStack {
-                Text(answer)
-                Button(action: flip) {
-                    Text("Show answer")
+                VStack {
+                    Spacer()
+                    ZStack() {
+                        Text(card.front)
+                            .placedOnCard(Color.yellow)
+                            .flipRotate(flipDegrees)
+                            .opacity(flipped ? 0.0 : 1.0)
+                        Text(card.back)
+                            .placedOnCard(Color.blue)
+                            .flipRotate(-180 + flipDegrees)
+                            .opacity(flipped ? 1.0 : 0.0)
+                    }
+                    .animation(.easeInOut(duration: 0.8), value: flipped)
+                    .onTapGesture {
+                        flipped.toggle()
+                    }
+                    Spacer()
                 }
                 .toolbar {
                     Button("Edit") {
@@ -38,29 +52,36 @@ struct CardView: View {
                 }
             }
         }
-        // Refresh UI after the sheet is dismissed
-        .onChange(of: isPresentingEditView) { _, isPresented in
-            if !isPresented {
-                answer = card.front
-            }
-        }
+    }
+}
+
+extension View {
+    func flipRotate(_ degrees : Double) -> some View {
+        return rotation3DEffect(Angle(degrees: degrees), axis: (x: 0.0, y: 1.0, z: 0.0))
     }
     
-    func flip() {
-        if answer == card.back {
-            answer = card.front
-        } else {
-            answer = card.back
-        }
+    func placedOnCard(_ color: Color) -> some View {
+        return padding(5)
+            .fontDesign(.serif)
+            .font(.system(size: 54))
+            .scaledToFit()
+            .lineLimit(2)
+            .minimumScaleFactor(0.01)
+            .containerRelativeFrame([.vertical, .horizontal])
+            .background(color)
+            .cornerRadius(12.0)
+            .shadow(radius: 12)
+            .border(.black)
+            
     }
 }
 
 #Preview(Card.sampleData.first?.front ?? "Card Preview", traits: .cardSampleData) {
     // Use the first sample card
-    if let first = Card.sampleData.first {
-        CardView(card: first)
-    } else {
+//    if let first = Card.sampleData.first {
+//        CardView(card: first)
+//    } else {
         // Fallback if sample data is empty
-        CardView(card: Card(front: "Front", back: "Back"))
-    }
+        CardView(card: Card(front: "Frontjasdasdsadsdasdsad", back: "Back"))
+//    }
 }
